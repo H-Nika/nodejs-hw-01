b614f88040e5229419082ee4f0c794ac25144f03
H-Nika <88vr07@gmail.com>
2024-06-05 13:25:41 +0300

    upd err1

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,17 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { log } from 'console';
 import { getAllContacts } from './getAllContacts.js';
 import fs from 'fs/promises';
 
 export const thanos = async () => {
       const contacts = await getAllContacts();
-  const data = [];
-  contacts.forEach((contact) => {
-    const random = Math.random() >= 0.5;
-    if (random) {
-      data.push(contact);
-    }
-  });
+  const data = contacts.filter(() => Math.random() >= 0.5);
+
   try {
     await fs.writeFile(PATH_DB, JSON.stringify(data));
   } catch (err) {
